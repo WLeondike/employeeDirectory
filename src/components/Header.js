51a236d9cf324c052e0ca55693a600c86a424067
WLeondike <billyleondike@gmail.com>
2020-08-09 12:39:37 -0400

    header and search bar added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <input type="text" placeholder="Employee Name?" />
+        </div>
+    )
+}
+
+export default Header;
